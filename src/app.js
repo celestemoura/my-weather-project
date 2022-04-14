516da1f886edddf52e48c870d0b2309698b4e2e4
celestemoura <celestemouras@gmail.com>
2022-04-14 18:56:48 +0200

    added forecast, removed movie-of-the-day card, changed layout

--- src/app.js
+++ src/app.js
@@ -4,15 +4,20 @@ function updateTimestamp(timestamp) {
   if (hours < 10) {
     hours = `0${hours}`;
   }
-
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   let weekday = date.getDay();
   let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return `${weekdays[weekday]} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000 + 86400000);
+  let day = date.getDay();
+  let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return weekdays[day];
 }
 
 function search(city) {
@@ -25,6 +30,13 @@ function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   search(city);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "d4e08a0b9b2ea184fab7dbd303ce7427";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function updateLocationAndWeatherConditions(response) {
@@ -73,6 +85,8 @@ function updateLocationAndWeatherConditions(response) {
   // Celsius link is .active on load
   celsiusTriggerClick.classList.add("active");
   fahrenheitTriggerClick.classList.remove("active");
+
+  getForecast(response.data.coord);
 }
 
 function locate(location) {
@@ -85,6 +99,34 @@ function findUser(position) {
   let apiKey = "d4e08a0b9b2ea184fab7dbd303ce7427";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateLocationAndWeatherConditions);
+}
+
+function displayForecast(response) {
+  forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weekly-forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col weekly-forecast">
+          <div>${formatDay(forecastDay.dt)}<br />${Math.round(
+          forecastDay.temp.min
+        )}°/<strong>${Math.round(forecastDay.temp.max)}°</strong><br />
+            <img
+              class="forecast-icon"
+              src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+            />
+          </div>`;
+      forecastHTML = forecastHTML + `</div>`;
+      forecastElement.innerHTML = forecastHTML;
+
+      minTemperatureCelsiusForecast = forecastDay.temp.min;
+      maxTemperatureCelsiusForecast = forecastDay.temp.max;
+    }
+  });
 }
 
 function convertToFahrenheit(event) {
@@ -101,8 +143,8 @@ function convertToFahrenheit(event) {
   )}°`;
 
   // convert min/max temperatures
-  minTemperatureFahrenheit = (minTemperatureCelsius * 9) / 5 + 32;
-  maxTemperatureFahrenheit = (maxTemperatureCelsius * 9) / 5 + 32;
+  let minTemperatureFahrenheit = (minTemperatureCelsius * 9) / 5 + 32;
+  let maxTemperatureFahrenheit = (maxTemperatureCelsius * 9) / 5 + 32;
   document.querySelector("#min-max").innerHTML = `${Math.round(
     minTemperatureFahrenheit
   )}°/${Math.round(maxTemperatureFahrenheit)}°`;
